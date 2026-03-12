acb8af0f088894c2510a1cd1b088ea964e7c023f
Hani-hue <isnahabibah8729@gmail.com>
2026-03-12 14:15:35 +0700

    tambah fungsi testing koneksi pada textBox1_TextChanged

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -28,7 +28,17 @@ namespace PraktikumADO
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
