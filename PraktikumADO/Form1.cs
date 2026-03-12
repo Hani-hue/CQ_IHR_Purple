9b2c0fd52d0e4bacb633868d0a901d6dddc42f73
Hani-hue <isnahabibah8729@gmail.com>
2026-03-12 13:20:41 +0700

    menambahkan text box "Hasil"

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -16,5 +16,10 @@ namespace PraktikumADO
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
